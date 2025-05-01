fae1379ac18dc2c04cee4b63a71bcfdc7d26519e
zeik <alexgermosen@gmail.com>
2025-04-30 21:13:43 -0400

    movido services/getIngetOut.ts -> routes/entryRoutes.ts

---
+++ backend/src/routes/entryRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { registerEntry, registerExit } from "../controllers/visitController";
+const router = Router();
+
+// Registrar entrada de visitas
+router.post("/visit/entrada", registerEntry);
+
+// Registrar salida de visitas
+router.put("/visit/salida/:qrId", registerExit);
+
+export default router;
