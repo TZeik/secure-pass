fab6f276d27cb764697440a3b59ac6463c006176
zeik <alexgermosen@gmail.com>
2025-04-30 22:05:56 -0400

    routers agregados a index.ts, visitController mod.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import entryRoutes from './routes/entryRoutes';
+import visitRoutes from './routes/visitRoutes';
+import qrRoutes from './routes/qrRoutes';
 
 const app = express();
 
@@ -14,6 +17,8 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(MONGODB_URI)
     .then(() => console.log('Se ha realizado la conexión con MongoDB Atlas'))
     .catch((err: Error) => console.error('Error al conectar a MongoDB Atlas: ', err));
+
+app.use('/api', visitRoutes, qrRoutes, entryRoutes);
 
 app.get('/', (req, res) => {
     res.send('SecurePass API');
